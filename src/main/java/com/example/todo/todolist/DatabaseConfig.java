28dedbc9a0fa6d48be06a2bc66b7dbe9c6417822
VitalikoV <drap877@gmail.com>
2020-10-25 12:36:41 +0200

    disconnection to db for heroku

--- src/main/java/com/example/todo/todolist/DatabaseConfig.java
+++ src/main/java/com/example/todo/todolist/DatabaseConfig.java
@@ -19,30 +19,30 @@ import java.util.Map;
 public class DatabaseConfig {
     @Value("${spring.database.url")
     private String dbUrl;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @RequestMapping("/db")
-    String db(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-            ArrayList<String> output = new ArrayList<String>();
-            while (rs.next()) {
-                output.add("Read from DB: " + rs.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "db";
-        } catch (Exception e) {
-            model.put("message", e.getMessage());
-            return "error";
-        }
-    }
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @RequestMapping("/db")
+//    String db(Map<String, Object> model) {
+//        try (Connection connection = dataSource.getConnection()) {
+//            Statement stmt = connection.createStatement();
+//            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+//            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//
+//            ArrayList<String> output = new ArrayList<String>();
+//            while (rs.next()) {
+//                output.add("Read from DB: " + rs.getTimestamp("tick"));
+//            }
+//
+//            model.put("records", output);
+//            return "db";
+//        } catch (Exception e) {
+//            model.put("message", e.getMessage());
+//            return "error";
+//        }
+//    }
 
     @Bean
     public DataSource dataSource(){
